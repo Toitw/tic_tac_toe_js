69bcc8e3363b8fa0ac5ea4a2d426815e8782f43e
juroga_new <juanjrgast@gmail.com>
2023-09-18 11:17:22 +0200

    Fixed resetBoard backend part so it let you populate the arrays after reseting the board

--- script.js
+++ script.js
@@ -118,11 +118,9 @@ const displayController = (() => {
             cell.textContent = "";
         });
         //Backend
-        gameBoard.board = [];
         for (let i = 0; i < 3; i++) {
-            gameBoard.board.push([]);
             for (let j = 0; j < 3; j++) {
-            gameBoard.board[i].push([]);
+            board[i][j] = [];
             }
         }
 
